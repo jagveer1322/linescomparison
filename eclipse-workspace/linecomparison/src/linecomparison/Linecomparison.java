73c307aa004eeb7fbe4330da8312de706d45790b
jagveer <jagveersingh.55555@gmail.com>
2022-03-04 20:08:58 +0530

    update commit uc1

--- eclipse-workspace/linecomparison/src/linecomparison/Linecomparison.java
+++ eclipse-workspace/linecomparison/src/linecomparison/Linecomparison.java
@@ -1,7 +1,5 @@
 package linecomparison;
-
 import java.util.Scanner;
-
 public class Linecomparison {
 	public static void main(String[] args) {
         System.out.println("Welcome Line Comparison");
